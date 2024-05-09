9b2159a5124d2d4cf782313238e15674cc673ced
alinosratipour <alinosratipour@gmail.com>
2024-05-09 09:48:49 +0100

    remove commend

--- front-end/src/apolloClient.ts
+++ front-end/src/apolloClient.ts
@@ -2,7 +2,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  //uri: 'http://localhost:5000/graphql', // Replace with your GraphQL server URL
   uri: "https://pizza-project-pagq.onrender.com/graphql",
   cache: new InMemoryCache(),
 });
