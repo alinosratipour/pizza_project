5a3ff72c8df12340baa2e099eb2e35c3b7d5cd16
alinosratipour <alinosratipour@gmail.com>
2024-05-16 12:30:08 +0100

    add order button to landing page

--- front-end/src/pages/Home/Home.tsx
+++ front-end/src/pages/Home/Home.tsx
@@ -1,7 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Home.scss";
+import Button from "../../components/UI-Liberary/Button/Button";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/pizza-menu");
+  };
   return (
     <div className="Home-Container">
       <div className="Hero-Pizza">
@@ -11,8 +16,13 @@ const Home = () => {
       <div className="Hero-Text-Container">
         <p className="text">Are you hungry ?</p>
         <Link to="/pizza-menu" className="Hero-Text">
-          Order Now
+          Stay Home We Deliver
         </Link>
+        <div className="Button-Container">
+          <Button size="md" colorscheme="secondery" onClick={handleClick}>
+            Order Now
+          </Button>
+        </div>
       </div>
     </div>
   );
