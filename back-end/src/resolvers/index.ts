6dff84513d7bf529178070721c29d95b17fca79c
alinosratipour <alinosratipour@gmail.com>
2024-06-14 14:15:13 +0100

    add user

--- back-end/src/resolvers/index.ts
+++ back-end/src/resolvers/index.ts
@@ -1,22 +1,28 @@
 import getAllPizzasListResolver from "./getAllPizzasListResolver";
 //import getpizzaWithRelatedToppings from "./getpizzaWithRelatedToppingsResolver";
 import getpizzaWithRelatedToppingsResolver from "./getpizzaWithRelatedToppingsResolver";
-//import getToppingPricesForSizesResolver from "./ getToppingPricesForSizesResolver ";
 import getpizzasWithSizesAndPricesResolver from "./getpizzasWithSizesAndPricesResolver";
 import getSizesWithBasesResolver from "./getSizesWithBasesResolver"
 import getToppingPricesBySizeResolver  from "./getToppingPricesBySizeResolver"
 import getBasesBySizeResolver from "./getBasesBySizeResolver"
 import getToppingsOnPizzaResolver from "./getToppingsOnPizzaResolver"
+import createUserResolver from "./createUserResolver";
+
+
 const resolvers = {
   Query: {
     getAllPizzasList:getAllPizzasListResolver,
     getpizzaWithRelatedToppings:getpizzaWithRelatedToppingsResolver,
-   // getToppingPricesForSizes: getToppingPricesForSizesResolver, 
     getpizzasWithSizesAndPrices: getpizzasWithSizesAndPricesResolver,
     getSizesWithBases:getSizesWithBasesResolver,
     getToppingPricesBySize:getToppingPricesBySizeResolver, 
     getBasesPricesBySize:getBasesBySizeResolver,
-    getToppingsOnPizza:getToppingsOnPizzaResolver
+    getToppingsOnPizza:getToppingsOnPizzaResolver,
+    
+
+  },
+  Mutation: {
+    ...createUserResolver.Mutation, // Merge in the createUser mutation resolver
 
   },
 };
